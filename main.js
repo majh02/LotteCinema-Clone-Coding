670b15b8f1f2b6a185ef3975f44c335a2a44cd39
MAJIHYE <majh0294@gmail.com>
2021-05-22 23:45:37 +0900

    Third Commit [2021-05-22]

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const header=document.querySelector('header');
+const header_length=header.clientHeight;
+const poster=document.getElementById('poster');
+
+let styles=getComputedStyle(poster);
+console.log(styles.getPropertyPriority('--header-size'));
+poster.style.setProperty('--header-size',-header_length);
+console.log(styles.getPropertyPriority('--header-size'));
+
+
 const closeBtn=document.querySelector('.banner .closeBtn');
 const banner=document.querySelector('.banner');
 
