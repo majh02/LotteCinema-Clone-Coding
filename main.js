de47b418d29cdd4c53f788834bbc30f8b8202242
MAJIHYE <majh0294@gmail.com>
2021-05-23 01:05:16 +0900

    4th Commit [2021-05-23]

--- main.js
+++ main.js
@@ -1,16 +1,11 @@
-const header=document.querySelector('header');
-const header_length=header.clientHeight;
-const poster=document.getElementById('poster');
-
-let styles=getComputedStyle(poster);
-console.log(styles.getPropertyPriority('--header-size'));
-poster.style.setProperty('--header-size',-header_length);
-console.log(styles.getPropertyPriority('--header-size'));
-
-
 const closeBtn=document.querySelector('.banner .closeBtn');
 const banner=document.querySelector('.banner');
 
 closeBtn.addEventListener('click',()=>{
     banner.remove();
+});
+
+const trailer=document.querySelector('.player button');
+trailer.addEventListener('click',()=>{
+    window.open("video.html","","height=480, width=854");
 });
