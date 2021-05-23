a7984ad80d33a1565e12c9ca697901173d7735b8
MAJIHYE <majh0294@gmail.com>
2021-05-23 13:36:27 +0900

    Heart button [2021-05-23]

--- main.js
+++ main.js
@@ -9,3 +9,18 @@ const trailer=document.querySelector('.player button');
 trailer.addEventListener('click',()=>{
     window.open("video/fastandfurious.mp4","","fullscreen");
 });
+
+var poster=document.querySelectorAll('.ranking img');
+for(var i=0;i<poster.length;i++){
+    poster[i].addEventListener('hover',()=>{
+        console.log('hover');
+    })
+}
+
+var heart_btn=document.querySelectorAll('.ranking .heart');
+function clickMenuHandler(){
+    this.classList.add('heart-active');
+}
+for(var i=0;i<heart_btn.length;i++){
+    heart_btn[i].addEventListener('click',clickMenuHandler);
+}
